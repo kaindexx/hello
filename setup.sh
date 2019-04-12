d1a9c575ae0421bfffef56d16934f78af9715d62
kain <49466764+kaindexx@users.noreply.github.com>
2019-04-12 09:39:44 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
 echo "ACTUALIZANDO SISTEMA"
-apt-get update
-apt-get upgrade
-apt-get install figlet cowsay fortune fortunes-es fortunes-es-off
+apt update
+apt upgrade
+apt install figlet cowsay fortune fortunes-es fortunes-es-off
 echo -n "DALE ENTER PARA INSTALAR PYTHON3:"
 read enter
 figlet python3
-apt-get install python3
+apt install python3
 echo -n "DALE ENTER PARA SALIR:"
 read enter
 cowsay -f tux adios....
